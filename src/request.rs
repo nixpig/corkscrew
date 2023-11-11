8f9a76aaf876531ab88d2e28d480be4595f15516
nixpig <143995476+nixpig@users.noreply.github.com>
2023-11-11 08:31:39 +0000

    wip: ensure not to filter out if nothing prrovided

--- src/request.rs
+++ src/request.rs
@@ -4,7 +4,13 @@ pub fn exec(config: &Config) {
     config.hosts.iter().for_each(|x| {
         x.requests
             .iter()
-            .filter(|x| config.request_names.contains(&x.name))
+            .filter(|x| {
+                if config.request_names.len() != 0 {
+                    config.request_names.contains(&x.name)
+                } else {
+                    true
+                }
+            })
             .for_each(|x| {
                 println!("send request: {:?}", x);
             });
