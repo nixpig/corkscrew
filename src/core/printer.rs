aea798678aafaa36b531865310f4246f05a3a7b0
nixpig <143995476+nixpig@users.noreply.github.com>
2023-12-23 05:46:07 +0000

    feat: print results as a table

--- src/core/printer.rs
+++ src/core/printer.rs
@@ -1,7 +1,20 @@
 use cli_table::{print_stdout, WithTitle};
 
+use crate::Output;
+
 pub fn print(results: Vec<reqwest::Response>) {
-    for result in results.iter() {
-        println!("{}", result.status());
+    let mut output: Vec<Output> = vec![];
+
+    for (i, result) in results.iter().enumerate() {
+        let url = result.url().as_str();
+        let status = result.status();
+
+        output.push(Output {
+            num: i,
+            url: String::from(url),
+            status: String::from(status.as_str()),
+        })
     }
+
+    let _ = print_stdout(output.with_title()).is_ok();
 }
--- src/types/output.rs
+++ src/types/output.rs
@@ -2,12 +2,18 @@
 pub struct Output {
     #[table(title = "#")]
     pub num: usize,
-    #[table(title = "Name")]
-    pub name: String,
-    #[table(title = "Method")]
-    pub method: String,
-    #[table(title = "Resource")]
-    pub resource: String,
+
+    // #[table(title = "Name")]
+    // pub name: String,
+
+    // #[table(title = "Method")]
+    // pub method: String,
+
+    // #[table(title = "Resource")]
+    // pub resource: String,
+    #[table(title = "URL")]
+    pub url: String,
+
     #[table(title = "Status")]
     pub status: String,
     // #[table(data = "Data")]
